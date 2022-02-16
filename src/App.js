a1e26420fd51bd2c2ffa2a4c5bdf947d2c161d2a
Wander Torres <wander_torres@live.com>
2022-02-16 11:11:08 -0300

    code cleanup

--- src/App.js
+++ src/App.js
@@ -5,65 +5,64 @@ import Card from './components/Card/Card';
 import CardDetails from './components/CardDetails/CardDetails';
 
 export default function App() {
+    const [listProducts, setProductList] = useState([]);
+    const [listSaved, setListSaved] = useState([]);
+    const [cart, setCart] = useState([]);
 
-  const [listProducts, setProductList] = useState([]);
-  const [listSaved, setListSaved] = useState([]);
-  const [cart, setCart] = useState([]);
-
-  const handleSaved = (id) => {
-    let list = [];
-    let aux = listSaved.find(item  => item.id === id);
-    
-    if(!aux) {
-      list = listProducts.filter(item  => item.id === id);
-      setListSaved(prevState => ([...prevState, ...list]));
-    } else {
-      list = listSaved.filter(item  => item.id !== id)
-      setListSaved(prevState => (prevState, list));
+    const handleSaved = (id) => {
+        let list = [];
+        let aux = listSaved.find(item  => item.id === id);
+        
+        if(!aux) {
+            list = listProducts.filter(item  => item.id === id);
+            setListSaved(prevState => ([...prevState, ...list]));
+        } else {
+            list = listSaved.filter(item  => item.id !== id)
+            setListSaved(prevState => (prevState, list));
+        }
     }
-  }
 
-  const handleCart = (id) => {
-    let list = [];
-    let aux = cart.find(item  => item.id === id);
-    
-    if(!aux) {
-      list = listProducts.filter(item  => item.id === id);
-      setCart(prevState => ([...prevState, ...list]));
-    } else {
-      list = cart.filter(item  => item.id !== id)
-      setCart(prevState => (prevState, list));
+    const handleCart = (id) => {
+        let list = [];
+        let aux = cart.find(item  => item.id === id);
+        
+        if(!aux) {
+            list = listProducts.filter(item  => item.id === id);
+            setCart(prevState => ([...prevState, ...list]));
+        } else {
+            list = cart.filter(item  => item.id !== id)
+            setCart(prevState => (prevState, list));
+        }
     }
-  }
 
-  const loadProducts = async () => {
-    let list = db;
-    setProductList(list);
-  }
+    const loadProducts = async () => {
+        let list = db;
+        setProductList(list);
+    }
 
-  useEffect(() => {
-    loadProducts();
-  }, []);
+    useEffect(() => {
+        loadProducts();
+    }, []);
 
-  return (
-    <div className="App">
-      <h2>Todos os produtos</h2>
-      <div className='divCard'>
-        {listProducts.map((item, key) => 
-          <Card key={key} product={item} handleSaved={handleSaved} handleCart={handleCart} />)}
-      </div>
-      
-      <h2>Produtos salvos</h2>
-      <div className='divCard'>
-        {listSaved.map((item, key) => 
-          <CardDetails key={key} product={item} />)}
-      </div>
+    return (
+        <div className="App">
+            <h2>Todos os produtos</h2>
+            <div className='divCard'>
+                {listProducts.map((item, key) => 
+                  <Card key={key} product={item} handleSaved={handleSaved} handleCart={handleCart} />)}
+            </div>
+            
+            <h2>Produtos salvos</h2>
+            <div className='divCard'>
+                {listSaved.map((item, key) => 
+                  <CardDetails key={key} product={item} />)}
+            </div>
 
-      <h2>Carrinho</h2>
-      <div className='divCard'>
-        {cart.map((item, key) => 
-          <CardDetails key={key} product={item} />)}
-      </div>
-    </div>
-  );
+            <h2>Carrinho</h2>
+            <div className='divCard'>
+                {cart.map((item, key) => 
+                  <CardDetails key={key} product={item} />)}
+            </div>
+        </div>
+    );
 }
--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -22,5 +22,5 @@ export default function CardDetails ({product}) {
                 </div>
             </div>
         </div>
-  );
+    );
 }
