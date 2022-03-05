edbf09f54138ddf9c5868c26927d505e948f7422
Wander Torres <wander_torres@live.com>
2022-03-05 02:46:14 -0300

    refactor style and title button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,31 +1,20 @@
 import { useState } from 'react';
 import './Button.css';
-import { FaCheck } from 'react-icons/fa';
 
 export default function Button (id: number) {
     const [active, setActive] = useState(false);
-    const [className, setClassName] = useState('button');
-    const [title, setTitle] = useState('Adicionar');
 
     const changeButton = (id: number) => {
         setActive(!active);
-
-        if(active) {
-            setTitle('Adicionar'); 
-            setClassName('button');
-        } else {
-            setTitle('Adicionado');
-            setClassName('button active');
-        }
 
         //handleCart(id);
     }
 
     return (
-        <button className={className} onClick={() => changeButton(id)}>
-            {active && 
-                <FaCheck />} 
-            {title}
+        <button 
+            className={ active ? "--active" : "" } 
+            onClick={() => changeButton(id)}>
+            { active ? "Adicionado" : "Adicionar" } 
         </button>
     );
 }
